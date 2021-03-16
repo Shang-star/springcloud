2be045ecaac92819d772d8f00a0ea33050f7cca2
shang <778670825@qq.com>
2021-03-16 23:00:15 +0800

    数据库测试用例

---
+++ db02.sql
@@ -0,0 +1,37 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50531
+ Source Host           : localhost:3306
+ Source Schema         : db02
+
+ Target Server Type    : MySQL
+ Target Server Version : 50531
+ File Encoding         : 65001
+
+ Date: 16/03/2021 22:55:28
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for dept
+-- ----------------------------
+DROP TABLE IF EXISTS `dept`;
+CREATE TABLE `dept`  (
+  `dept_no` bigint(20) NOT NULL,
+  `dept_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `db_source` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`dept_no`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of dept
+-- ----------------------------
+INSERT INTO `dept` VALUES (1001, '员工1', 'db02');
+INSERT INTO `dept` VALUES (1002, '员工2', 'db02');
+
+SET FOREIGN_KEY_CHECKS = 1;
